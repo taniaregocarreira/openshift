81e2c88cbc46a89800fd9400dbbf13bdcc2bbfe3
Sebastian Colomar <SEBASTIAN.COLOMAR@GMAIL.COM>
2019-06-07 08:51:50 +0200

    Comment unnecessary commands

--- Shell/hellojava.sh
+++ Shell/hellojava.sh
@@ -1,5 +1,5 @@
 docker image build --tag hellojava https://github.com/secobau/openshift/raw/master/Dockerfile/hellojava
-docker image ls
-docker image history hellojava
+#docker image ls
+#docker image history hellojava
 docker container run hellojava
 docker container run hellojava -help
